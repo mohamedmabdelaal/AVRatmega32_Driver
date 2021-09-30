c2431c3d5c22cca15731aad5e565ca92a647a9a3
mohamedmabdelaal <91672265+mohamedmabdelaal@users.noreply.github.com>
2021-09-30 23:08:36 +0200

    Add files via upload

---
+++ 01-MCAL/01-DIO/DIO_interface.h
@@ -0,0 +1,31 @@
+#ifndef _DIO_INTERFACE_H
+#define _DIO_INTERFACE_H
+
+#define		PORTA	0
+#define		PORTB	1
+#define		PORTC	2
+#define		PORTD	3
+
+#define		HIGH	1
+#define		LOW		0
+
+#define		INPUT	0
+#define		OUTPUT	1
+
+#define		PIN0	0
+#define		PIN1	1
+#define		PIN2	2
+#define		PIN3	3
+#define		PIN4	4
+#define		PIN5	5
+#define		PIN6	6
+#define		PIN7	7
+
+
+void DIO_voidSetPinDirection(u8 PortID , u8 PinID , u8 Direction);
+void DIO_voidSetPinValue(u8 PortID , u8 PinID , u8 Value);
+u8	DIO_u8GetPinValue(u8 PortID , u8 PinID);
+void DIO_voidSetPortDirection(u8 PortID , u8 Direction);
+void DIO_voidSetPortValue(u8 PortID ,  u8 Value);
+
+#endif
